d52e71886355bbf0b79e4a956e36c9ac3d512e5d
Abnercuellar <abner2120@hotmail.com>
2022-02-23 17:20:34 -0500

    probando con foreach

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const JSONToCSV = require("json2csv");
 const FileSystem = require("fs");
 
 CSVToJSON().fromFile("./covid19.csv").then(source => {
-    var covid19 = [''];
+    var covid19 = [""];
+    var estado1 = [""]
     var covid19 = JSON.parse(JSON.stringify(source));
-    console.log(covid19[1]);
+    // console.log(covid19[1]);
+    console.time();
+    covid19.forEach(element => console.log(element));
+    console.timeEnd();
 });
 
