0a1833e6d0c8eec3799e22d36f5e94fd7b0bcde5
Abnercuellar <abner2120@hotmail.com>
2022-02-24 08:06:07 -0500

    no lo subire

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ CSVToJSON().fromFile("./covid19.csv").then(source => {
     var estado1 = [""]
     var covid19 = JSON.parse(JSON.stringify(source));
     // console.log(covid19[1]);
-    console.time();
-    covid19.forEach(element => console.log(element));
-    console.timeEnd();
+    // covid19.forEach(element => console.log(element));
 });
 
